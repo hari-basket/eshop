7a4b2a036101bdbfaadd59d06b830ddf0d50e0aa
ND <reachoutnilesh@outlook.com>
2021-06-20 05:32:15 +0530

    project initialization

---
+++ environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://hari-basket.ap-south-1.elasticbeanstalk.com/api/v1/',
+};
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/hari-basket',
+    '<rootDir>/apps/admin',
+    '<rootDir>/libs/ui',
+    '<rootDir>/libs/users',
+    '<rootDir>/libs/orders',
+    '<rootDir>/libs/products',
+  ],
+};
