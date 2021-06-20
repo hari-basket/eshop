8a9603c1b1e9347447dc8cc5b09da22b87a32027
ND <reachoutnilesh@outlook.com>
2021-06-20 06:39:25 +0530

    added use hash

--- apps/admin/src/app/app.route.module.ts
+++ apps/admin/src/app/app.route.module.ts
@@ -61,7 +61,12 @@ const routes: Routes = [
   },
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled',
+      useHash: true,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
